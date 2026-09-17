2946918ecaa585a493c2d21d0ff29aeae111a709
Icaro Nery <icaro.alesnery1@gmail.com>
2026-09-17 01:28:49 -0300

    chore(backend): configura Spring Boot

---
+++ backend/src/main/java/com/imfinances/IMFinancesApplication.java
@@ -0,0 +1,11 @@
+package com.imfinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMFinancesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IMFinancesApplication.class, args);
+    }
+}
